f5ddb3d7320977857cf264cc99c442795807aefb
Michelle <mmccully@s3.com>
2010-11-11 20:19:58 -0600

    Created two seed users- bart@test.com has trader role and lisa@test.com has admin role.  Both use password of password.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,16 @@ Item.create(:color=> "Yellow", :desc => "Pencil")
 Item.create(:color=> "Blue", :desc => "Pencil")
 Item.create(:color=> "Blue", :desc => "Expo Marker")
 Item.create(:color=> "Black", :desc => "Pilot Pen")
+
+User.delete_all
+User.create(:first_name=> "Bart",
+            :last_name=> "Simpson",
+            :email=> "bart@test.com",
+            :password => "password",
+            :roles => :trader)
+          
+User.create(:first_name=> "Lisa",
+            :last_name=> "Simpson",
+            :email=> "lisa@test.com",
+            :password => "password",
+            :roles => :admin)
